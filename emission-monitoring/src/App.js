155b57da225018516e6a223a87b877dd03bf948c
Shubham Chakraborty <55108926+ENIGMA-KUN@users.noreply.github.com>
2024-07-05 05:59:26 -0700

    main

--- emission-monitoring/src/App.js
+++ emission-monitoring/src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
 import './App.css';
-import EmissionChart from './EmissionChart';
+import Graph from './components/Graph';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sustain Cipher Emission Monitoring Analysis</h1>
-      </header>
-      <EmissionChart />
-    </div>
+   <>
+   <h1>Emitrix Analytics</h1>
+   <Graph/>
+   </>
   );
 }
 
